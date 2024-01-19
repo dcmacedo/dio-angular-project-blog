acb22e54e681ce5cf9fbf5b974fc2f710888ad6e
Danilo Cotrim Macedo <dcotrimmacedo@gmail.com>
2024-01-18 22:44:55 -0300

    :sparkles: feat: criamos os componentes principais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { FooterCardComponent } from './components/footer-card/footer-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MenuBarComponent, MenuTitleComponent, BigCardComponent, SmallCardComponent, FooterCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
